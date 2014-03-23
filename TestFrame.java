e207186630d2f85bb648c6fbddee8a20f2a7a73a
3345678 <abc7778527@yahoo.com.tw>
2014-03-23 11:58:57 +0800

    Test and Add the Mainmethod
    I try to change some numbers but the result doesn't have any change
    I don't know why
    And I add the Button and a TextField for GUI demand
    And the Main method I create  1000 : 625 size.

--- TestFrame.java
+++ TestFrame.java
@@ -2,19 +2,43 @@ import javax.swing.JLabel;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import java.awt.GridLayout;
-
+import javax.swing.JButton;
 
 public class TestFrame extends JFrame {
 	public TestFrame() {
-		setLayout(new GridLayout(5,3,7,7));
+		setLayout(new GridLayout(5,3,5,5));
 		
 		add(new JLabel("Name"));
-		add(new JTextField());
+		add(new JTextField(5));
 		add(new JLabel("Age"));
-		add(new JTextField());
+		add(new JTextField(2));
 		add(new JLabel("Height"));
-		add(new JTextField());
+		add(new JTextField(7));
 		add(new JLabel("Weight"));
+		add(new JTextField(5));
+		add(new JButton("Enter"));
 		add(new JTextField());
 	}
 	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+      TestFrame frame = new TestFrame();
+      frame.setTitle("GUI");
+      frame.setSize(1000,625);
+      frame.setLocationRelativeTo(null);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+		
+		
+		
+		
+		
+		
+				
+	}
+}
