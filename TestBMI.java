6590f90678db697d5c8bc7c5571b3ba0773e7a45
3345678 <abc7778527@yahoo.com.tw>
2014-04-04 14:58:07 +0800

    NewOne
    Type the object in need

---
+++ TestBMI.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TestBMI extends javax.swing.JFrame {
+	private JButton Enter;
+	private JLabel label1;
+	private JLabel label2;
+	private JLabel label3;
+	private JLabel label4;
+	private JLabel label5;
+	private JLabel label6;
+	private JLabel label7;
+	private JLabel label8;
+	private JLabel label9;
+	private JLabel label10;
+	private JLabel label11;
+	private JLabel label12;
+	private JTextField BMI;
+	private JTextField Name;
+	private JTextField Age;
+	private JTextField Height;
+	private JTextField Weight;
+	private String name;
+	private int age;
+	private double weight; 
+	private double height; 
+	public static final double KILOGRAMS_PER_POUND = 0.45359237;
+	public static final double METERS_PER_INCH = 0.0254;
+	private double bmi;
+	private String status;
